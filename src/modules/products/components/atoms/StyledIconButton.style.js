4ce620abc60f6f86209899b42d5a67015b2de79b
Kamila Dynysiuk <kamila.dynysiuk@bcf-software.pl>
2021-07-29 13:57:24 +0200

    change name for expandedOpen

--- src/modules/products/components/atoms/StyledIconButton.style.js
+++ src/modules/products/components/atoms/StyledIconButton.style.js
@@ -1,18 +1,20 @@
 import { IconButton, styled } from "@material-ui/core";
 
-const StyledIconButton = styled(IconButton)(({ theme, open, isCard }) => ({
-  transform: "rotate(0deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-  ...(open && { transform: "rotate(180deg)" }),
-  ...(isCard && {
-    marginLeft: 0,
-    justifyContent: "start",
-    width: "45px",
-    backgroundColor: "red",
-  }),
-}));
+const StyledIconButton = styled(IconButton)(
+  ({ theme, expandedOpen, isCard }) => ({
+    transform: "rotate(0deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+    ...(expandedOpen && { transform: "rotate(180deg)" }),
+    ...(isCard && {
+      marginLeft: 0,
+      justifyContent: "start",
+      width: "45px",
+      backgroundColor: "red",
+    }),
+  })
+);
 
 export default StyledIconButton;
