0f0925b3cb72a83e53c6eea158eaea6053f83405
Kamila Dynysiuk <kamila.dynysiuk@bcf-software.pl>
2021-08-09 15:03:55 +0200

    add marin 0 when card is open

--- src/shared/layout/Main/Main.style.js
+++ src/shared/layout/Main/Main.style.js
@@ -1,8 +1,10 @@
 import { Container, styled } from "@material-ui/core";
 
-export const StyledContainer = styled(Container)(({ isCardOpen }) => ({
+export const StyledContainer = styled(Container)(({ theme, isCardOpen }) => ({
   width: "100%",
   ...(isCardOpen && {
     maxWidth: "calc(100% - 380px)",
+    marginLeft: theme.spacing(0),
+    marginRight: theme.spacing(0),
   }),
 }));
