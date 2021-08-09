26b7913247cfca94bffc7bc6efc0fecefdeb8cc0
Kamila Dynysiuk <kamila.dynysiuk@bcf-software.pl>
2021-08-09 15:01:17 +0200

    remove console.log

--- src/shared/layout/Main/Main.js
+++ src/shared/layout/Main/Main.js
@@ -2,7 +2,6 @@ import React from "react";
 import { StyledContainer } from "./Main.style";
 
 const Main = ({ children, open }) => {
-  console.log(open);
   return <StyledContainer isCardOpen={open}>{children}</StyledContainer>;
 };
 export default Main;
