7390609c04f40585883e5d902bd219cf5cee79aa
kamila.dynysiuk <kamila.dynysiuk@bcf-software.pl>
2021-10-09 15:35:01 +0200

    add navlink

--- src/modules/vendor/shared/components/Sidebar/Sidebar.js
+++ src/modules/vendor/shared/components/Sidebar/Sidebar.js
@@ -1,34 +1,35 @@
 import React from "react";
-import { Box, Link } from "@material-ui/core";
+import { Box } from "@material-ui/core";
 import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import ListAltIcon from "@material-ui/icons/ListAlt";
+import { useGetProducts } from "../../../../../shared";
 import { routes } from "../../../../../config/routes";
-import { StyledCard } from "./Sidebar.style";
+import { StyledCard, activeStyle, StyledNavLink } from "./Sidebar.style";
 
 const Sidebar = () => {
+  const { products } = useGetProducts();
   return (
     <StyledCard variant='outlined'>
-      <Box display='flex' alignItems='center' mb={3}>
-        <AddCircleOutlineIcon />
-        <Link href={routes.addNewProduct} color='inherit'>
-          Dodaj nowy produkt
-        </Link>
-      </Box>
-      <Box
-        display='flex'
-        alignItems='center'
-        justifyContent='space-between'
-        mb={3}
-      >
-        <Box display='flex'>
-          <ListAltIcon />
-          <Link href={routes.products} color='inherit'>
-            Produkty
-          </Link>
+      <StyledNavLink to={routes.addNewProduct} activeStyle={activeStyle}>
+        <Box display='flex' alignItems='center' mb={3}>
+          <AddCircleOutlineIcon />
+          <span>Dodaj nowy produkt</span>
         </Box>
-
-        <span>100</span>
-      </Box>
+      </StyledNavLink>
+      <StyledNavLink to={routes.products} activeStyle={activeStyle}>
+        <Box
+          display='flex'
+          alignItems='center'
+          justifyContent='space-between'
+          mb={3}
+        >
+          <Box display='flex'>
+            <ListAltIcon />
+            <span> Produkty</span>
+          </Box>
+          <span>{products.length}</span>
+        </Box>
+      </StyledNavLink>
     </StyledCard>
   );
 };
