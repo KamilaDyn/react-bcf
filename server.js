a6e6501c294b9892860b7f5bee90c2a613459a16
kamila.dynysiuk <kamila.dynysiuk@bcf-software.pl>
2021-10-14 12:35:45 +0200

    edit server.js

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const path = require("path");
-const express = require("express");
+const router = jsonServer.router("data/db.json");
+
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router("db.json");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 4000;
 
-server.use("/db", middlewares, router);
-server.use(express.static(path.join(__dirname, "build")));
-
-server.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+server.use(middlewares);
+server.use(router);
 
 server.listen(port);
