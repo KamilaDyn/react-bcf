1764ebc467772ada276eaf3d0b360cb1d63edb76
kamila.dynysiuk <kamila.dynysiuk@bcf-software.pl>
2021-11-05 14:07:32 +0100

    remove getProduct from products selectors

--- src/store/product/selectors.js
+++ src/store/product/selectors.js
@@ -1,11 +1,9 @@
 const getProducts = (state) => state.products.products;
-const getProduct = (state) => state.products.product;
 const getSuccessMessage = (state) => state.products.successMessage;
 const getErrorMessage = (state) => state.products.errorMessage;
 
 export const productSelectors = {
   getProducts,
-  getProduct,
   getSuccessMessage,
   getErrorMessage,
 };
