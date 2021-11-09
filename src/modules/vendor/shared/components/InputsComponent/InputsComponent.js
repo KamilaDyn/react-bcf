040a3d2c6b62cd8309039ac984bc8cd53ee07319
kamila.dynysiuk <kamila.dynysiuk@bcf-software.pl>
2021-11-09 09:25:35 +0100

    change key name

--- src/modules/vendor/shared/components/InputsComponent/InputsComponent.js
+++ src/modules/vendor/shared/components/InputsComponent/InputsComponent.js
@@ -37,7 +37,7 @@ const InputsComponent = () => {
         <Grid item xs={6}>
           <FormControl>
             <Field
-              key={values.category}
+              key={`${values.category}-1`}
               component={Select}
               name='category'
               defaultValue={values.category || categories[0]}
