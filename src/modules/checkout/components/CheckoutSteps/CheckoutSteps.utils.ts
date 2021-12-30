237243dffe4a05b601b0b33aa571b042faed3b1a
kamila.dynysiuk <kamila.dynysiuk@bcf-software.pl>
2021-12-30 07:42:49 +0100

    add enum

--- src/modules/checkout/components/CheckoutSteps/CheckoutSteps.utils.ts
+++ src/modules/checkout/components/CheckoutSteps/CheckoutSteps.utils.ts
@@ -1,6 +1,6 @@
-export const steps = [
-  '1. Karta',
-  '2. Szczegóły',
-  '3. Płatność',
-  '4. Podsumowanie',
-];
+export enum steps {
+  card = '1.Karta',
+  details = '2.Szczegóły',
+  payments = '3.Płatność',
+  sum = '4.Podsumowanie',
+}
